80aa9715b030d81b96d1dd250c6d099e18abe010
Brady <brady.cunningham@ryerson.ca>
2022-04-11 05:21:44 -0400

    Initial commit

---
+++ js/swiper.js
@@ -0,0 +1,11 @@
+const swiper = new Swiper(".swiper", {
+    loop: true,
+    autoplay: {
+        delay: 4000,
+        disableOnInteraction: false,
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+})
