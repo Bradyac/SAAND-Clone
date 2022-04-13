7f9c56ce766ae1ae1d2c86397952749e2873f46a
Brady <brady.cunningham@ryerson.ca>
2022-04-13 11:11:02 -0400

    Re-enable autoplay on top banner

--- js/swiper.js
+++ js/swiper.js
@@ -1,9 +1,9 @@
 const swiper1 = new Swiper(".mySwipper1", {
     loop: true,
-    // autoplay: {
-    //     delay: 4000,
-    //     disableOnInteraction: false,
-    // },
+    autoplay: {
+        delay: 4000,
+        disableOnInteraction: false,
+    },
     navigation: {
         nextEl: ".swiper-button-next",
         prevEl: ".swiper-button-prev",
